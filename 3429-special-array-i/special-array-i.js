926df7ef07c6e9f72a1587dc59092619948e50ec
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2025-09-13 11:21:32 +0300

    Time: 69 ms (0.50%) | Memory: 63.9 MB (7.50%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isArraySpecial = function (nums) {
+    let p = 0, q = 1;
+
+    if (nums.length === 1) return true;
+
+    while (p < nums.length && q < nums.length) {
+        const first = nums[p];
+        const second = nums[q];
+
+        console.log(isEven(first) && isOdd(second), p, q);
+
+        if ((isOdd(first) && isOdd(second)) || (isEven(first) && isEven(second))) {
+            return false;
+        }
+
+        p++;
+        q++;
+    }
+
+    return true;
+};
+
+const isOdd = (n) => {
+    return n % 2 !== 0;
+}
+
+const isEven = (n) => {
+    return n % 2 === 0;
+}
