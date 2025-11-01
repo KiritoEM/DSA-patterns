4dd7c53feda6ef4dbbed7a600e432ac44abd2bc0
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2025-11-01 11:00:29 +0300

    Time: 120 ms (44.44%) | Memory: 80.6 MB (50.00%) - LeetSync

---
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSquareStreak = function (nums) {
+    let set = new Set(nums.sort((a, b) => a - b));
+    let streakCount = 0;
+
+    set.forEach((val) => {
+        let subsequence = [val];
+        let current = val;
+
+        while (set.has(current * current)) {
+            current = current * current;
+            subsequence.push(current);
+        }
+
+        if (subsequence.length >= 5) {
+            streakCount = 5;
+            return;
+        }
+
+        streakCount = Math.max(streakCount, subsequence.length);
+    })
+
+    return streakCount > 1 ? streakCount : -1;
+};
