a410e7a535bc304fecc93dd1814159a0e6eebc35
Loick Emadisson <146085343+KiritoEM@users.noreply.github.com>
2025-11-02 08:44:18 +0300

    Time: 2 ms (37.84%) | Memory: 55.1 MB (97.30%) - LeetSync

---
+++ 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isFascinating = function (n) {
+    let concatenatedNumber = n.toString() + (n * 2) + (n * 3);
+    let set = new Set();
+
+    for (let val of concatenatedNumber) {
+        if (!(val >= 1 && val <= 9) || set.has(parseInt(val))) {
+            return false;
+        }
+
+        set.add(parseInt(val));
+    }
+
+    return true;
+};
